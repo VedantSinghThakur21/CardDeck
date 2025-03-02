40c7a10246fd9081a23abf57ea3d256893f845f3
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-02 09:49:36 +0530

    Added Deck Function to Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,11 @@
-public class Deck {
+import java.util.*;
+
+class Deck {
+    ArrayList<Card> deck;
+    Random random;
+
+    public Deck() {
+        deck = new ArrayList<>();
+        random = new Random();  // Initializing random
+    }
 }
