639465b66ec1f45474f06a72eef76670452bbef7
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-02 13:42:47 +0530

    Adding getters getSuit and getRank to Card.java in ref to findCard in deck.java

--- src/Card.java
+++ src/Card.java
@@ -4,9 +4,18 @@ class Card {
     String rank;
     String suit;
 
-    public Card(String rank, String suit){
+    public Card(String rank, String suit){ //Constructor
         this.rank = rank;
         this.suit = suit;
+    }
+
+    // Getters
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
     }
 
 }
