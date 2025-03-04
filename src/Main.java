7f93cc0a4e7a3670954ef3a3b94d19553d5e4666
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-04 18:14:05 +0530

    Debug - Added Creation of Deck

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Deck deck = new Deck();
+        deck.createDeck();
         int choice;
 
         do {
