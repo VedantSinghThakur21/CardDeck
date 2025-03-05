e39cf6da286b1f193b099dafb9f4957f33acd393
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-05 14:42:06 +0530

    Adding Comments to Card.java for better understanding and readability

--- src/Card.java
+++ src/Card.java
@@ -1,35 +1,40 @@
 import java.util.*;
 
-class Card {
-    String rank;
-    String suit;
 
-    public Card(String rank, String suit){ //Constructor
-        this.rank = rank;
-        this.suit = suit;
+//Class representing a single playing Card
+class Card {
+    String rank; //Stores the Rank of Card
+    String suit; //Stores the Suit of Card
+
+    //Constructor to Initialise the card with a specific rank and suit
+    public Card(String rank, String suit){
+        this.rank = rank; //assigns given rank to the card
+        this.suit = suit; //Assigns given suit to the card
     }
 
-    // Getters
+    // Getter method to get the suit of card
     public String getSuit() {
         return suit;
     }
 
+    //Getter method to get the rank of the card
     public String getRank() {
         return rank;
     }
 
-    // Check if two cards have the same rank
+    // method to check if two cards have the same rank
     public boolean sameRank(Card other) {
         return this.rank.equals(other.rank);
     }
 
-    //Check if two cards have same suit
+    //method to check if two cards have same suit
     public boolean sameSuit(Card other) {
         return this.suit.equals(other.suit);
     }
 
+    //method to print all details of a card
     public void printCard() {
-        System.out.println(rank + " of " + suit);
+        System.out.println(rank + " of " + suit); //Prints the card in "Rank of Suit" Format
     }
 
 }
